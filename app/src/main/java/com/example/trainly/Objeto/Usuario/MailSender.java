f5e972462fc189d601252d35655f9581c16d8067
Pablo <pgomezm2002@gmail.com>
2026-05-21 12:59:10 +0200

    prevencion de errores

--- app/src/main/java/com/example/trainly/Objeto/Usuario/MailSender.java
+++ app/src/main/java/com/example/trainly/Objeto/Usuario/MailSender.java
@@ -14,8 +14,8 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
 public class MailSender {
-    private static final String SENDER_EMAIL    = "oculto";
-    private static final String SENDER_PASSWORD = "oculto";
+    private static final String SENDER_EMAIL    = "email";
+    private static final String SENDER_PASSWORD = "contraseña";
 
     public interface MailCallback {
         void onSuccess();
